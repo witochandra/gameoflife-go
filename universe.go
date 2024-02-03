d11110e8b855267fbb24485a97f54fbb5bc13512
Wito Chandra <wito.chandra@gojek.com>
2024-02-03 13:53:43 +0700

    10mins

--- universe.go
+++ universe.go
@@ -10,6 +10,45 @@ func (u Universe) CellPositions() [][]int {
 	return u.cellPositions
 }
 
+/*
+Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by overpopulation.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+*/
+func (u Universe) Next() Universe {
+	cellByCoordinate := make(map[string]int)
+	for _, pos := range u.cellPositions {
+		coordinateStr := fmt.Sprintf("%d,%d", pos[0], pos[1])
+		cellByCoordinate[coordinateStr] = 1
+	}
+	coordinatesToCheckUniq := make(map[string]bool)
+	coordinatesToCheck := make([][]int, 0)
+	for _, pos := range u.cellPositions {
+		newCoordinates := [][]int{
+			pos,
+			{pos[0], pos[0] + 1},
+			{pos[0] + 1, pos[0] + 1},
+			{pos[0] + 1, pos[0]},
+			{pos[0] + 1, pos[0] - 1},
+			{pos[0], pos[0] - 1},
+			{pos[0] - 1, pos[0] - 1},
+			{pos[0] - 1, pos[0]},
+			{pos[0] - 1, pos[0] + 1},
+		}
+
+		for _, nCoordinate := range neighborCoordinates {
+			neighborsCount += cellByCoordinate[fmt.Sprintf("%d,%d", nCoordinate[0], nCoordinate[1])]
+		}
+		if neighborsCount == 3 {
+
+		}
+	}
+	newCellCoordinates := make([][]int, 0)
+
+	return NewUniverse(newCellCoordinates)
+}
+
 func NewUniverse(positions [][]int) Universe {
 	// removes duplicate cells in the input
 	cellPositionsUniq := make(map[string]bool)
