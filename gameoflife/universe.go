c545c2b20eb0dcfdd6c20fa7bc0525dd24848cc7
Wito Chandra <wito.c.91@gmail.com>
2024-03-01 07:41:57 +0000

    refactor: naming

--- gameoflife/universe.go
+++ gameoflife/universe.go
@@ -15,39 +15,39 @@ Any live cell with more than three live neighbors dies, as if by overpopulation.
 Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 */
 func (u Universe) Next() Universe {
-	cellByCoordinate := make(map[string]int)
+	cellByPoint := make(map[string]int)
 	for _, pos := range u.cellPositions {
-		cellByCoordinate[pos.String()] = 1
+		cellByPoint[pos.String()] = 1
 	}
-	coordinatesToCheckUniq := make(map[string]bool)
-	coordinatesToCheck := make([]Point, 0)
+	pointsToCheckUniq := make(map[string]bool)
+	pointsToCheck := make([]Point, 0)
 	for _, pos := range u.cellPositions {
-		newCoordinates := append([]Point{pos}, pos.Neighbors()...)
+		newPoints := append([]Point{pos}, pos.Neighbors()...)
 
-		for _, nCoordinate := range newCoordinates {
-			if coordinatesToCheckUniq[nCoordinate.String()] {
+		for _, nPoint := range newPoints {
+			if pointsToCheckUniq[nPoint.String()] {
 				continue
 			}
-			coordinatesToCheckUniq[nCoordinate.String()] = true
-			coordinatesToCheck = append(coordinatesToCheck, nCoordinate)
+			pointsToCheckUniq[nPoint.String()] = true
+			pointsToCheck = append(pointsToCheck, nPoint)
 		}
 	}
-	newCellCoordinates := make([]Point, 0)
-	for _, coordinate := range coordinatesToCheck {
-		neighbors := coordinate.Neighbors()
+	newCellPosition := make([]Point, 0)
+	for _, point := range pointsToCheck {
+		neighbors := point.Neighbors()
 
 		neighborCellsCount := 0
-		for _, nCoordinate := range neighbors {
-			neighborCellsCount += cellByCoordinate[nCoordinate.String()]
+		for _, nPoint := range neighbors {
+			neighborCellsCount += cellByPoint[nPoint.String()]
 		}
 
-		if (cellByCoordinate[coordinate.String()] == 1 && (neighborCellsCount == 2 || neighborCellsCount == 3)) ||
-			(cellByCoordinate[coordinate.String()] == 0 && neighborCellsCount == 3) {
-			newCellCoordinates = append(newCellCoordinates, coordinate)
+		if (cellByPoint[point.String()] == 1 && (neighborCellsCount == 2 || neighborCellsCount == 3)) ||
+			(cellByPoint[point.String()] == 0 && neighborCellsCount == 3) {
+			newCellPosition = append(newCellPosition, point)
 		}
 	}
 
-	return NewUniverse(newCellCoordinates)
+	return NewUniverse(newCellPosition)
 }
 
 func NewUniverse(positions []Point) Universe {
