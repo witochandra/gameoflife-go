718dd229fdcdd80f4cfedc360e400470f68e2306
Wito Chandra <wito.c.91@gmail.com>
2024-03-29 07:29:17 +0000

    refactor: add points.String

--- gameoflife/points.go
+++ gameoflife/points.go
@@ -1,8 +1,15 @@
 package gameoflife
 
+import "strings"
+
+const (
+	DeadCell  = '_'
+	AliveCell = '#'
+)
+
 type Points []Point
 
-func (ps Points) Edges() (bottomLeft, topRight Point) {
+func (ps Points) Edges() (topLeft, bottomRight Point) {
 	set := false
 	minX, minY, maxX, maxY := 0, 0, 0, 0
 	for _, p := range ps {
@@ -25,4 +32,44 @@ func (ps Points) Edges() (bottomLeft, topRight Point) {
 		}
 	}
 	return Point{minX, minY}, Point{maxX, maxY}
+}
+
+func (ps Points) String(offset int, emptyPoint, cellPoint string) string {
+	topLeft, bottomRight := ps.Edges()
+
+	lines := make([]string, 0)
+	if len(ps) > 0 {
+		pointByStr := make(map[string]int)
+		for _, p := range ps {
+			pointByStr[p.String()] = 1
+		}
+		for y := topLeft.Y; y <= bottomRight.Y; y++ {
+			row := ""
+			for x := topLeft.X; x <= bottomRight.X; x++ {
+				pos := Point{X: x, Y: y}
+				if pointByStr[pos.String()] == 1 {
+					row += cellPoint
+				} else {
+					row += emptyPoint
+				}
+			}
+			lines = append(lines, row)
+		}
+	}
+	if offset > 0 {
+		for i, line := range lines {
+			lines[i] = strings.Repeat(emptyPoint, offset) + line + strings.Repeat(emptyPoint, offset)
+		}
+		length := (bottomRight.X - topLeft.X) + (2 * offset)
+		if len(lines) > 0 {
+			length += 1
+		}
+		topDown := strings.Repeat(emptyPoint, length)
+		for i := 0; i < offset; i++ {
+			lines = append([]string{topDown}, lines...)
+			lines = append(lines, topDown)
+		}
+	}
+
+	return strings.Join(lines, "\n") + "\n"
 }
--- gameoflife/points_test.go
+++ gameoflife/points_test.go
@@ -2,19 +2,20 @@ package gameoflife_test
 
 import (
 	"gameoflife/gameoflife"
+	"strings"
 	"testing"
 )
 
 func TestPoints_Edges(t *testing.T) {
 	t.Run("Empty points", func(t *testing.T) {
 		ps := gameoflife.Points{}
-		bottomLeft, topRight := ps.Edges()
-		if bottomLeft != (gameoflife.Point{}) {
-			t.Errorf("Expected {0, 0}, got %v", bottomLeft)
+		topLeft, bottomRight := ps.Edges()
+		if topLeft != (gameoflife.Point{}) {
+			t.Errorf("Expected {0, 0}, got %v", topLeft)
 		}
 
-		if topRight != (gameoflife.Point{}) {
-			t.Errorf("Expected {0, 0}, got %v", topRight)
+		if bottomRight != (gameoflife.Point{}) {
+			t.Errorf("Expected {0, 0}, got %v", bottomRight)
 		}
 	})
 
@@ -25,13 +26,82 @@ func TestPoints_Edges(t *testing.T) {
 			{X: 2, Y: 1},
 		}
 
-		bottomLeft, topRight := ps.Edges()
-		if bottomLeft != (gameoflife.Point{X: -2, Y: 1}) {
-			t.Errorf("Expected {-2, 1}, got %v", bottomLeft)
+		topLeft, bottomRight := ps.Edges()
+		if topLeft != (gameoflife.Point{X: -2, Y: 1}) {
+			t.Errorf("Expected {-2, 1}, got %v", topLeft)
 		}
 
-		if topRight != (gameoflife.Point{X: 2, Y: 2}) {
-			t.Errorf("Expected {2, 2}, got %v", topRight)
+		if bottomRight != (gameoflife.Point{X: 2, Y: 2}) {
+			t.Errorf("Expected {2, 2}, got %v", bottomRight)
 		}
 	})
+}
+
+func TestPoints_String(t *testing.T) {
+	testCases := []struct {
+		name     string
+		points   gameoflife.Points
+		offset   int
+		expected string
+	}{
+		{
+			name:     "Empty points",
+			points:   gameoflife.Points{},
+			offset:   0,
+			expected: "\n",
+		},
+		{
+			name:     "Empty points with offset",
+			points:   gameoflife.Points{},
+			offset:   2,
+			expected: "____\n____\n____\n____\n",
+		},
+		{
+			name: "Single point",
+			points: gameoflife.Points{
+				{X: 1, Y: 1},
+			},
+			offset:   0,
+			expected: "#\n",
+		},
+		{
+			name: "Single point with offset",
+			points: gameoflife.Points{
+				{X: 1, Y: 1},
+			},
+			offset: 2,
+			expected: strings.Join([]string{
+				"_____",
+				"_____",
+				"__#__",
+				"_____",
+				"_____",
+			}, "\n") + "\n",
+		},
+		{
+			name: "Multiple points with offset",
+			points: gameoflife.Points{
+				{X: -1, Y: -1},
+				{X: 1, Y: 1},
+			},
+			offset: 2,
+			expected: strings.Join([]string{
+				"_______",
+				"_______",
+				"__#____",
+				"_______",
+				"____#__",
+				"_______",
+				"_______",
+			}, "\n") + "\n",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := tc.points.String(tc.offset, "_", "#")
+			if tc.expected != actual {
+				t.Errorf("Expected %s, got %s", tc.expected, actual)
+			}
+		})
+	}
 }
